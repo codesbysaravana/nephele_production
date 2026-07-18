8b115f9f2994fe429b1ec1b34aa6ea3a97eee41f
Saravana Priyan <c.saravanapriyan@gmail.com>
2026-07-18 12:19:06 +0530

    vercel route fix

--- frontend/main.js
+++ frontend/main.js
@@ -52,7 +52,7 @@ function setupVoicePage() {
     function connectionSocket() {
         return new Promise((resolve, reject) => {
             ws = new WebSocket("wss://nephele-dsoa.onrender.com/ws/audio");
-            //ws = new WebSocket("ws://localhost:8000/ws/audio");
+            // ws = new WebSocket("ws://localhost:8000/ws/audio");
             ws.onopen = () => {
                 wsStatus.innerHTML = 'Status: Connected to Server';
                 startBtn.disabled = true;
