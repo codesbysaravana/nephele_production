a4c9567563cf7f791eadc1a50c468803d5de4944
Saravana Priyan <c.saravanapriyan@gmail.com>
2026-09-06 20:55:21 +0530

    added changed repo

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://127.0.0.1:8000';
+export const API_BASE_URL = 'https://nephele-production.onrender.com';
 
 export const WS_BASE_URL = import.meta.env.VITE_WS_URL ||
     (API_BASE_URL.startsWith('https')
